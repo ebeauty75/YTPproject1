99b5b8891063b25f971e11eb917e077d9ad7404a
ebeauty75 <63450158+ebeauty75@users.noreply.github.com>
2022-12-25 07:41:55 -0800

    Added new files

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+// Hero Slider
+	$('#hero-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    items:1,
+    responsive:{
+        0:{
+            
+        },
+        600:{
+            
+        },
+        1000:{
+            
+        }
+    }
+})
+
+});
